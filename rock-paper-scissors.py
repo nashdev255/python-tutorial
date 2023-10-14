d40e14aa82a9ed0bbbfa3426a892db53c883387e
nashdev255 <yutori1107@gmail.com>
2023-10-15 00:16:35 +0900

    Update rock-paper-scissors.py

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,3 +1,14 @@
-hands = ['rock', 'paper', 'scissors']
+import random
 
-hand = input('')
+hands = ['グー', 'チョキ', 'パー']
+
+def start_battle(player_hand):
+  enemy_hand = random.randint(0, 3)
+  print('相手は' + hands[enemy_hand] + 'を出しました')
+  if player_hand == enemy_hand:
+    print('引き分けです')
+    return
+
+print('グー : 0, チョキ : 1, パー : 2')
+player_hand = int(input('出す手を入力 : '))
+start_battle(player_hand)
